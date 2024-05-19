9d5a5066470b028e45d0cdbebdbd47b5ec045993
Abhay Gupta <111756624+thisabhay@users.noreply.github.com>
2024-05-19 08:26:17 +0530

    Create javafoundation13.js

---
+++ 0-100/Week 1/practice-stuff/javafoundation13.js
@@ -0,0 +1,7 @@
+const currdate=new Date();
+console.log(currdate.getDate());
+console.log(currdate.getMonth());
+console.log(currdate.getYear());
+console.log(currdate.getFullYear());
+console.log(currdate.getHours()+":"+currdate.getMinutes()+":"+currdate.getSeconds());
+console.log(currdate.getTime());
