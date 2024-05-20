45a04422dab778147234249281347bd0937e279f
Abhay Gupta <111756624+thisabhay@users.noreply.github.com>
2024-05-20 19:09:44 +0530

    Create main.js

---
+++ 0-100/Week 1/01-js/main.js
@@ -0,0 +1,8 @@
+let str="Helloabcd ";
+console.log("Replace",str.replace('l','t'));
+console.log("Replace",str.replace('l','t'));
+console.log("Repeat",str.repeat(3));
+str=str.split('');
+console.log(str);
+str=str.sort();
+console.log(str);
